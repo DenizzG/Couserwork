c89c312f6eb0921bd32109b435da26db4147b0ba
DenizzG <64283539+DenizzG@users.noreply.github.com>
2020-11-05 21:54:06 +0000

    coursework

---
+++ WSTask3.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+PCarMil = int(input("How many miles was the car on before? = "))
+NCarMil = int(input("How many miles is the car on now? = "))
+tot = int(input("What is the total number of litres taken to fill the tank? = "))
+
+Miles = NCarMil - PCarMil
+anw = Miles/tot
+print("you car does ", anw," miles per gallon")
+=======
+PCarMil = int(input("How many miles was the car on before? = "))
+NCarMil = int(input("How many miles is the car on now? = "))
+tot = int(input("What is the total number of litres taken to fill the tank? = "))
+
+Miles = NCarMil - PCarMil
+anw = Miles/tot
+print("you car does ", anw," miles per gallon")
+
+##ACS - Logically correcvt
+##ACS - Code needs annotations (comments)
+>>>>>>> 923698615c989ade9d14ee55bb4c08e1fc9c8bc7
---
+++ WSTask4.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+Nst = int(input("How many students are there? = "))
+Nbk = int(input("How many books are there? = "))
+
+Books_per_s = int(Nst % Nbk)
+remaining = Nbk %Nst
+
+print("Each student gets " , Books_per_s, " books and there are ", remaining, " books left over.")
+=======
+Nst = int(input("How many students are there? = "))
+Nbk = int(input("How many books are there? = "))
+
+Books_per_s = int(Nst % Nbk)
+remaining = Nbk %Nst
+
+print("Each student gets " , Books_per_s, " books and there are ", remaining, " books left over.")
+
+## ACS - I don't think this works??? Haqve another look
+>>>>>>> 923698615c989ade9d14ee55bb4c08e1fc9c8bc7
